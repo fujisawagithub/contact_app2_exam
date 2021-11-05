2c1bf8544a78ceaec753f1f7c1fc874889da5c62
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-05 11:56:24 +0900

    params設定まで

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
   def index
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    Contact.create(contact_params)
+    redirect_to new_contact_path
+  end
+  
+  praivate
+  def contact_params
+    params.require(:contact).permit(:title , :content)
+    redirect_to new_contac
+  end
+
 end
